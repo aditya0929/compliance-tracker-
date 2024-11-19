15c39d23822944edad36a0214022765aa73279c1
Aditya  Jha <127277877+aditya0929@users.noreply.github.com>
2024-11-20 02:19:05 +0530

    Update compliance_tracker.py

--- compliance_tracker.py
+++ compliance_tracker.py
@@ -9,12 +9,11 @@ conn = sqlite3.connect("compliance_tracker.db")
 
 cursor = conn.cursor()
 
-TWILIO_ACCOUNT_SID = st.secrets["TWILIO_ACCOUNT_SID"]
-TWILIO_AUTH_TOKEN = st.secrets["TWILIO_AUTH_TOKEN"]
-TWILIO_PHONE_NUMBER = st.secrets["TWILIO_PHONE_NUMBER"]
-recipient_phone_number = st.secrets["RECIPIENT_PHONE_NUMBER"]
-
-client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+# Twilio Credentials (Use your own credentials)
+TWILIO_ACCOUNT_SID = "AC3d0179d31bda1e4dde5e4eaa9de9edea" 
+TWILIO_AUTH_TOKEN = "3c613bee5dee896ea708f5170c5b744c"  
+TWILIO_PHONE_NUMBER = "+15865018392"  
+recipient_phone_number = "+919153831641"  
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 # Function to send SMS
